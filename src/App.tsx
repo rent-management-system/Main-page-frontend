7a1480b4156331b3c7877a3d22849430e85cdeef
dagi <dagiteferi2011@gmail.com>
2025-11-01 21:13:33 +0300

    fix the nav

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import Properties from "./components/Properties.tsx";
 import About from "./components/About.tsx";
 import Contact from "./components/Contact.tsx";
 import { MyProvider } from "./context/MyContext.tsx";
+import Header from "./components/Header.tsx";
 import { Route, Routes } from "react-router-dom";
 import { StyledEngineProvider } from "@mui/material/styles";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
-import Header from "./components/Header.tsx";
+
 import "./style.scss";
 
 function App() {
@@ -22,7 +23,7 @@ function App() {
     <StyledEngineProvider injectFirst>
       <MyProvider>
         <div id="bd">
-          <Header />
+          <HeaderV2 />
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/vehicles" element={<Properties />}></Route>
